5f8734b495eb16a0a8dab935f191a128fe80fcb9
ddrichmond1998 <drichmondhealthexec@gmail.com>
2021-11-21 17:36:13 -0600

    commit

--- src/cors-enabled/cors-enabled.router.js
+++ src/cors-enabled/cors-enabled.router.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const controller = require("./cors-enabled.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors");
+
+
 
 router
   .route("/:corsId")
@@ -11,7 +14,7 @@ router
 
 router
   .route("/")
-  .get(controller.list)
+  .get(cors(), controller.list)
   .post(controller.create)
   .all(methodNotAllowed);
 
